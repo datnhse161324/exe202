e5dfb5c34292c3a497c4294430691baa19e69ee7
Hue <huetqse161477@fpt.edu.vn>
2023-10-11 18:53:50 +0700

    map input fix

--- app/src/main/java/com/example/exe201/OrderActivity.java
+++ app/src/main/java/com/example/exe201/OrderActivity.java
@@ -1,15 +1,16 @@
 package com.example.exe201;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class OrderActivity extends AppCompatActivity {
 
     Button btnChoose;
+    TextView location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,15 +18,14 @@ public class OrderActivity extends AppCompatActivity {
         btnChoose= findViewById(R.id.selectLoca);
         Intent intent= getIntent();
         String username= intent.getStringExtra("user");
-
-        btnChoose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent= new Intent(getApplicationContext(), MapActivity.class);
-                intent.putExtra("user",username);
-                startActivity(intent);
-                finish();
-            }
+        location = findViewById(R.id.tvAddress);
+        btnChoose.setOnClickListener(view -> {
+            Intent intent1 = new Intent(getApplicationContext(), MapActivity.class);
+            intent1.putExtra("user",username);
+            intent1.putExtra("address", location.getText());
+            startActivity(intent1);
         });
+
+        location.setText(getIntent().getStringExtra("location"));
     }
 }
